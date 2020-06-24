1513d46ff1f339c56d91c3726d6baf151e0a66ad
Seongjin Yoo <yoosngj@gmail.com>
2020-06-25 01:54:49 +0900

    feat : Modify project file

---
+++ src/main/java/me/dev/oliver/youtubesns/YoutubeSnsApplication.java
@@ -0,0 +1,13 @@
+package me.dev.oliver.youtubesns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeSnsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(YoutubeSnsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/dev/oliver/youtubesns/YoutubeSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dev.oliver.youtubesns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeSnsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
