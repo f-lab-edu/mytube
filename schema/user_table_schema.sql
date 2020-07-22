d720835ffa5b80536bbcb8dfdf665faf1f4a7839
Seongjin Yoo <yoosngj@gmail.com>
2020-07-22 11:00:48 +0900

    Modify "tel_num -> phone" valiable name

--- schema/user_table_schema.sql
+++ schema/user_table_schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE user
     `pw`           CHAR(20)        NOT NULL    COMMENT '비밀번호', 
     `name`         CHAR(20)        NOT NULL    COMMENT '이름', 
     `email`        CHAR(45)        NOT NULL    COMMENT '이메일', 
-    `addr`	       VARCHAR(100)    NOT NULL    COMMENT '도로 주소', 
+    `addr`	   VARCHAR(100)    NOT NULL    COMMENT '도로 주소', 
     `signup_date`  date        	   NOT NULL    COMMENT '가입 날짜', 
-    `tel_num`      CHAR(45)        NOT NULL    COMMENT '전화 번호', 
+    `phone`        CHAR(45)        NOT NULL    COMMENT '전화 번호', 
     PRIMARY KEY (id, user_id)
 );
