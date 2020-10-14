748523d2a15389ef9c36c6f855922e66baabc7f9
Seongjin Yoo <yoosngj@gmail.com>
2020-10-14 15:49:05 +0900

    Modify update date & created date variable name

--- schema/youtube_sns_schema.sql
+++ schema/youtube_sns_schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE user
     `name`         CHAR(20)        NOT NULL    COMMENT '이름',
     `email`        CHAR(45)        NOT NULL    COMMENT '이메일',
     `addr`         VARCHAR(100)    NOT NULL    COMMENT '주소',
-    `signup_date`  CHAR(45)        NOT NULL    COMMENT '가입 날짜',
+    `createdAt`    CHAR(45)        NOT NULL    COMMENT '가입 날짜',
     `phone`        VARCHAR(45)     NOT NULL    COMMENT '핸드폰',
     PRIMARY KEY (id),
     UNIQUE (user_id)
@@ -23,7 +23,8 @@ CREATE TABLE board
     `user_id`          CHAR(20)        NOT NULL    COMMENT '유저 아이디', 
     `title`            VARCHAR(100)    NOT NULL    COMMENT '제목', 
     `detail_contents`  VARCHAR(200)    NOT NULL    COMMENT '세부 내용', 
-    `reg_date`         CHAR(45)        NOT NULL    COMMENT '등록 날짜', 
+    `createdAt`        CHAR(45)        NOT NULL    COMMENT '등록 날짜',
+    `updatedAt`        CHAR(45)        NOT NULL    COMMENT '업데이트 날짜',
     `like_count`       INT             NOT NULL    COMMENT '좋아요 개수', 
     `bad_count`        INT             NOT NULL    COMMENT '싫어요 개수', 
     `hits`             INT             NOT NULL    COMMENT '조회수', 
