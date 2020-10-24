1a79c30e4915a7e826477ea400fb1a7a4bffffa6
Seongjin Yoo <yoosngj@gmail.com>
2020-10-24 21:27:49 +0900

    [#13] Modify upload video contents
    Modify log comment

--- src/main/java/me/dev/oliver/youtubesns/service/VideoServiceImpl.java
+++ src/main/java/me/dev/oliver/youtubesns/service/VideoServiceImpl.java
@@ -21,12 +21,13 @@ public class VideoServiceImpl implements VideoService {
   /**
    * 동영상 업로드, file size는 byte 단위로 저장됨 동영상 컨텐츠 내의 세부사항 기록 db에 저장
    *
-   * @param multipartFile 게시물 관련 정보를 담은 객체
-   * @param userId 회원 아이디
-   * @param title 동영상 제목
+   * @param multipartFile  게시물 관련 정보를 담은 객체
+   * @param userId         회원 아이디
+   * @param title          동영상 제목
    * @param detailContents 동영상에 대한 세부 내용
    */
-  public void uploadVideo(MultipartFile multipartFile, String userId, String title, String detailContents) {
+  public void uploadVideo(MultipartFile multipartFile, String userId, String title,
+      String detailContents) {
 
     String fileName = multipartFile.getOriginalFilename();
 
@@ -47,7 +48,8 @@ public class VideoServiceImpl implements VideoService {
       videoMapper.insertVideo(videoDto);
       videoMapper.insertDetailInfo(videoDto);
     } catch (IOException e) {
-      log.error("insertVideo 메서드에서 file 처리 중 에러가 발생했습니다, 자세한 에러 내용 : " + e);
+      log.error("insertVideo 메서드에서 file 처리 중 에러가 발생했습니다, 자세한 에러 내용 : ", e, " LinNumber: ",
+          e.getStackTrace()[0].getLineNumber());
       throw new IllegalStateException("서버에서 파일 처리중 예상치 못한 에러가 발생했습니다");
     }
   }
