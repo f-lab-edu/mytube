ba875ecc3aabd00e40563b788e8343696ff0e440
Seongjin Yoo <yoosngj@gmail.com>
2020-11-02 00:05:51 +0900

    [#13] Modify upload video contents
    Add LocaDateTime and Date diff comment

--- src/main/java/me/dev/oliver/mytubesns/dto/VideoWatchDto.java
+++ src/main/java/me/dev/oliver/mytubesns/dto/VideoWatchDto.java
@@ -4,6 +4,12 @@ import java.time.LocalDateTime;
 import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 
+/**
+ * LocalDateTime vs Date
+ * Date타입은 mutable하고 LocalDateTime은 immutable함. 가변 클래스인 Date는 deprecated되었고
+ * LocalDateTime, LocalDate, LocalTime은 Java8에 추가 됨. Date 클래스는 Depensive copying하기 위해 밖으로 객체를 내보낼 때
+ * 새로운 객체를 만들어 사용했지만 Java8에서 추가된 LocalDateTime은 immutable하여 값이 변경 되지 않아 사용하는데 안전.
+ */
 @Getter
 public class VideoWatchDto {
 
