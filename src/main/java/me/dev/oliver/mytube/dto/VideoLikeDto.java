556511fe2866dacd6e5166f02e16ccd7c91cf205
Seongjin Yoo <yoosngj@gmail.com>
2020-11-05 17:41:28 +0900

    [#15] Modify like dislike program
    Modify VideoLikeDto class

--- src/main/java/me/dev/oliver/mytube/dto/VideoLikeDto.java
+++ src/main/java/me/dev/oliver/mytube/dto/VideoLikeDto.java
@@ -5,13 +5,7 @@ import lombok.Getter;
 @Getter
 public class VideoLikeDto {
 
-  private final int videoId;
+  private int videoId;
 
-  private final String userId;
-
-  public VideoLikeDto(int videoId, String userId) {
-    this.videoId = videoId;
-    this.userId = userId;
-  }
-
+  private String userId;
 }
